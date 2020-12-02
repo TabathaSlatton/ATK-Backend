50fa108d50945efd96a42bc9586b5a82166faaf7
Tabatha Slatton <valbatha@gmail.com>
2020-12-01 19:36:59 -0700

    started bcrypt and serialization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+
+
+
 
 end
