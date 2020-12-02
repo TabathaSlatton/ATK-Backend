231ef2d4fde625a0eda2d48d34097782c70bd8a7
Tabatha Slatton <valbatha@gmail.com>
2020-12-01 23:07:34 -0700

    added login functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,16 @@
 class Api::V1::UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
+    def profile
+        # using current_user helper in ApplicationController
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+      end
+
     def create
-    #   byebug
       @user = User.create(user_params)
       if @user.valid?
         # passing in payload of user id
         @token = encode_token(user_id: @user.id)
-        # byebug
         # using built-in rails status codes
         render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
       else
