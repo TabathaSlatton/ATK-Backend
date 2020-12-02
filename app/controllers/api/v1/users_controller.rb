792bf6ff343cb686f1e7fc4783b3df0790766bbe
Tabatha Slatton <valbatha@gmail.com>
2020-12-01 21:51:58 -0700

    set up backend jwt

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
     def create
+    #   byebug
       @user = User.create(user_params)
       if @user.valid?
-        render json: { user: UserSerializer.new(@user) }, status: :created
+        # passing in payload of user id
+        @token = encode_token(user_id: @user.id)
+        # byebug
+        # using built-in rails status codes
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
       else
         render json: { error: 'failed to create user' }, status: :not_acceptable
       end
